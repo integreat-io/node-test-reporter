746d4305db5dbd9e5bf0cabd367d1ba59fd0095d
Kjell-Morten <post@kjellmorten.no>
2025-02-22 16:21:11 +0100

    Lint

--- tests/helpers/runTests.js
+++ tests/helpers/runTests.js
@@ -1,8 +1,8 @@
 // import { tap } from "node:test/reporters";
-import { run } from "node:test";
-import reporter from "../../lib/index.js";
+import { run } from 'node:test'
+import reporter from '../../lib/index.js'
 
-const __dirname = import.meta.dirname.slice(0, -8); // Remove the last 8 chars -- refering to the helpers folder
+const __dirname = import.meta.dirname.slice(0, -8) // Remove the last 8 chars -- refering to the helpers folder
 
 /**
  * Run the given test file (in the `/tests/cases` folder), and return output as
@@ -12,12 +12,12 @@ export default async function runTest(testFiles) {
   // Run the test
   const stream = run({
     files: testFiles.map((file) => `${__dirname}/cases/${file}.js`),
-  }).compose(reporter);
+  }).compose(reporter)
 
   // Retrieve and return the output from the test
-  const result = [];
+  const result = []
   for await (let chunk of stream) {
-    result.push(chunk);
+    result.push(chunk)
   }
-  return result.filter(Boolean);
+  return result.filter(Boolean)
 }
