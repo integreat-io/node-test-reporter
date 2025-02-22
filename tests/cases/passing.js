774599219b2a355bf4fc1f0846b3be129eee4b49
Kjell-Morten <post@kjellmorten.no>
2025-02-22 17:46:42 +0100

    Format with prettier

--- tests/cases/passing.js
+++ tests/cases/passing.js
@@ -1,6 +1,6 @@
-import test from "node:test";
-import assert from "node:assert/strict";
+import test from 'node:test'
+import assert from 'node:assert/strict'
 
-test("should pass", () => {
-  assert.equal(true, true);
-});
+test('should pass', () => {
+  assert.equal(true, true)
+})
